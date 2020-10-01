a711a1d621a22a01edd35cfe69e9fa975ea12ba3
nikhilm19 <nikmul19@gmail.com>
2020-10-02 00:07:59 +0530

    Links

---
+++ src/components/Main/Landing/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import ControlledCarousel from "../Carousel";
+import ContactUs from "../Contact-Us";
+import Features from "../Features";
+
+import Services from "../Services";
+import WhyUs from "../Why-Us";
+
+import Pricing from "../Pricing";
+
+export default () => {
+  return (
+    <>
+      <Features />
+      <WhyUs />
+      <Services />
+      <Pricing />
+      <ContactUs />
+    </>
+  );
+};
